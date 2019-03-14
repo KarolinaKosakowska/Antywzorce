a801a48a17d1494f3a8d8d27f680e9a99a161977
KarolinaKosakowska <karolina.kosakowska.email@gmail.com>
2019-03-14 18:29:25 +0100

    New Repo

--- Antywzorce/Program.cs
+++ Antywzorce/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Antywzorce.Spagetti;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,14 @@ namespace Antywzorce
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Zamawiam kawę; dużą 200ml; Z mlekiem; 50ml; Z cukrem; 2 kostki.");
+            var coffee = new Drink().Create();
+            Console.WriteLine(coffee);
+
+            Console.Read();
+            //Wynik:            
+            //Kawa; duża 200 ml; z mlekiem 50 ml; z cukrem 2 kostki. 
         }
     }
 }
---
+++ Antywzorce/Spagetti/Drink.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antywzorce.Spagetti
+{
+    public class Drink
+    {
+        IDrink drink;
+        int Napoj, Wielkosc, Mleko, IleMleka, Cukier, IleCukru;
+        public Drink(IDrink drink)
+        {
+            this.drink = drink;
+        }
+         
+        public void CreateDrink()
+        {
+            drink.SelectSize();
+
+        }
+    }
+       
+}
